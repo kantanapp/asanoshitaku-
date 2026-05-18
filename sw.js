06d216c475fdfb1700dff4ced74d80e4ecdd07d6
shinichi <shinichi@shinichinoMacBook-Pro.local>
2026-05-18 13:55:20 +0900

    ボタン横幅を400pxに拡大

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'asajunbi-v202605181345';
+const CACHE_NAME = 'asajunbi-v202605181355';
 const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
