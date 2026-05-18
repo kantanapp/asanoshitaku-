806d09253f3a7ec7b0f91ed77e7842a7cd48b764
shinichi <shinichi@shinichinoMacBook-Pro.local>
2026-05-18 13:45:50 +0900

    ボタンを横並び・画面いっぱいに拡大

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'asajunbi-v202605181332';
+const CACHE_NAME = 'asajunbi-v202605181345';
 const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
