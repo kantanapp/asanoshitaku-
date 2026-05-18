2cdf99a5654ae77d840493d5f6293c49dfa95da1
shinichi <shinichi@shinichinoMacBook-Pro.local>
2026-05-18 14:15:00 +0900

    スワイプでページ間移動できるよう拡張

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'asajunbi-v202605181403';
+const CACHE_NAME = 'asajunbi-v202605181415';
 const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
