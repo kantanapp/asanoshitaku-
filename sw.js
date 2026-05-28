a40a6e09bdb9517beb24b52c892b59313c3ba96f
shinichi <shinichi@shinichinoMacBook-Pro.local>
2026-05-28 19:33:07 +0900

    Google Analytics 設置・全画面計測対応

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'asajunbi-v202605181516';
+const CACHE_NAME = 'asajunbi-v202605281933';
 const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
